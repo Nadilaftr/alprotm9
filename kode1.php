bdb628d99e4a0fef053faa7692db0ad93e4007a7
Nadilaftr <118698890+Nadilaftr@users.noreply.github.com>
2023-11-10 00:15:33 +0700

    Update kode1.php

--- kode1.php
+++ kode1.php
@@ -3,7 +3,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $id = $_POST['id'];
     $F_name = $_POST['F_name'];
     $L_name = $_POST['L_name'];
-// karena nama header di tabel itu F_name sama L_name, jadi pokoknya semua samain kayak gitu//
     $email = $_POST['email'];
     $email2 = $_POST['email2'];
     $profesi = $_POST['profesi'];
